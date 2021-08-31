7adec18cc5ae51d989734fab08416f639e86e9f3
Ash Lai <z0975201508@gmail.com>
2021-08-31 14:15:15 +0800

    add media queries for images and fix error in Slogan text

--- src/app/containers/HomePage/topSection.tsx
+++ src/app/containers/HomePage/topSection.tsx
@@ -4,9 +4,10 @@ import tw from 'twin.macro';
 
 import MclarenCarImg from "../../../assets/images/mclaren-orange-big.png";
 import BlobImg from "../../../assets/images/blob.svg";
+import { SCREENS } from '../../components/responsive';
 
 const TopSectionContainer = styled.div`
-  min-height: 68px;
+  min-height: 400px;
   margin-top: 6em;
   ${tw`
     w-full
@@ -81,6 +82,30 @@ const BlobContainer = styled.div`
     height: auto;
     max-height: max-content;
   }
+
+  @media (min-width: ${SCREENS.sm}) {
+    width: 40em;
+    max-height: 10em;
+    right: -9em;
+    top: -16em;
+    transform: rotate(-25deg);
+  }
+
+  @media (min-width: ${SCREENS.lg}) {
+    width: 50em;
+    max-height: 30em;
+    right: -7em;
+    top: -15em;
+    transform: rotate(-30deg);
+  }
+
+  @media (min-width: ${SCREENS.xl}) {
+    width: 70em;
+    max-height: 30em;
+    right: -15em;
+    top: -25em;
+    transform: rotate(-20deg);
+  }
 `;
 
 const StandaloneCar = styled.div`
@@ -95,13 +120,31 @@ const StandaloneCar = styled.div`
     height: 100%;
     max-width: fit-content;
   }
+
+  @media (min-width: ${SCREENS.sm}) {
+    height: 16em;
+    right: -6em;
+    top: -6em;
+  }
+
+  @media (min-width: ${SCREENS.lg}) {
+    height: 21em;
+    right: -8em;
+    top: -5em;
+  }
+
+  @media (min-width: ${SCREENS.xl}) {
+    height: 30em;
+    right: -13em;
+    top: -9em;
+  }
 `;
 
 export function TopSection() {
   return (
     <TopSectionContainer>
       <LeftContainer>
-        <Slogan>Rent The Best Quality Car;s With Us</Slogan>
+        <Slogan>Rent The Best Quality Car's With Us</Slogan>
         <Description>
           Always choose the best car from our local stores or order it remotely
           at the best price for you and get the best quality cars for as long as
